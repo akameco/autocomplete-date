688c5a4b96d5d1aa45108636fdeb8474b4f93208
akameco <akameco.t@gmail.com>
2016-11-10 18:46:58 +0900

    Fix format

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const provider = {
 		} else if (/^(time|dt)$/.test(prefix)) {
 			text = moment().format('HH:mm:ss')
 		} else if (/^now$/.test(prefix)) {
-			text = moment().format('YYYY-MM-DD hh:mm:ss')
+			text = moment().format('YYYY-MM-DD HH:mm:ss')
 		} else {
 			return Promise.resolve(null);
 		}
